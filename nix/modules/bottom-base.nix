84bf394dec3d2964c85deef67c3a227c1e694658
Robin Walter <hello@robinwalter.me>
2026-05-29 18:39:51 +0200

    feat(modules): create base module for `programs.bottom`
    Signed-off-by: Robin Walter <hello@robinwalter.me>

---
+++ nix/modules/bottom-base.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2026 Robin Walter <hello@robinwalter.me>
+# SPDX-License-Identifier: MIT
+#
+_: {
+  programs.bottom = {
+    enable = true;
+
+    settings = { };
+  };
+}
