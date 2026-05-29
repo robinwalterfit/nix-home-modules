f834581b841d2387e5b3b34e06534a2a812871db
Robin Walter <hello@robinwalter.me>
2026-05-29 17:48:04 +0200

    feat(lib): create file to hold meta information about the project
    Signed-off-by: Robin Walter <hello@robinwalter.me>

---
+++ nix/lib.nix
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 Robin Walter <hello@robinwalter.me>
+# SPDX-License-Identifier: MIT
+#
+# Meta information
+{ self, ... }:
+{
+  projectName = "nix-home-modules";
+
+  # Use (short) git revision hash or dirty, if there are uncommitted changes
+  #
+  # See https://discourse.nixos.org/t/flakes-accessing-selfs-revision/11237/8
+  rev = toString (self.rev or self.dirtyRev or self.lastModified or "unknown");
+  shortRev = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
+}
