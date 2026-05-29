1ec2a9ba89444d6b1e3c45f1789b9c34d8298b4c
Robin Walter <hello@robinwalter.me>
2026-05-29 18:37:32 +0200

    feat(modules): create base module for `services.colima`
    Signed-off-by: Robin Walter <hello@robinwalter.me>

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
         lib = import ./nix/lib.nix { inherit self; };
 
         homeModules = {
+          colima-base = ./nix/modules/colima-base.nix;
           direnv-base = ./nix/modules/direnv-base.nix;
           eza-base = ./nix/modules/eza-base.nix;
           fastfetch-base = ./nix/modules/fastfetch-base.nix;
