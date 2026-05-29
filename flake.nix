5f0949947a124b0c4453ab0014f178ce70fc28a8
Robin Walter <hello@robinwalter.me>
2026-05-29 17:50:18 +0200

    feat(flake): create project flake and dev partition flake
    Signed-off-by: Robin Walter <hello@robinwalter.me>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: 2026 Robin Walter <hello@robinwalter.me>
+# SPDX-License-Identifier: MIT
+#
+{
+  description = "Reusable Home Manager Modules";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    # See: https://flake.parts/getting-started.html
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { self, ... }:
+      let
+        lib = import ./nix/lib.nix { inherit self; };
+
+        homeModules = { };
+      in
+      {
+        systems = [ ];
+
+        imports = [
+          # Activate partitions
+          flake-parts.flakeModules.partitions
+        ];
+
+        partitionedAttrs = {
+          checks = "dev";
+          devShells = "dev";
+          formatter = "dev";
+        };
+        partitions = {
+          dev = {
+            extraInputsFlake = ./nix/dev;
+            module =
+              { ... }:
+              {
+                imports = [ ];
+              };
+          };
+        };
+
+        flake = { inherit homeModules lib; };
+      }
+    );
+}
