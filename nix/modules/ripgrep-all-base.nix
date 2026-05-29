73d59bdd9ae92151d2583da259c7fb0ea03c9e0f
Robin Walter <hello@robinwalter.me>
2026-05-29 18:46:38 +0200

    feat(modules): create base module for `programs.ripgrep-all`
    Signed-off-by: Robin Walter <hello@robinwalter.me>

---
+++ nix/modules/ripgrep-all-base.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2026 Robin Walter <hello@robinwalter.me>
+# SPDX-License-Identifier: MIT
+#
+_: {
+  programs.ripgrep-all = {
+    enable = true;
+
+    custom_adapters = [ ];
+  };
+}
