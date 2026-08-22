43b384fda209d8e2b55b28018b7d47220cd22e65
Robin Walter <hello@robinwalter.me>
2026-08-22 14:29:07 +0200

    chore(deps): update flake inputs of `dev` partition to latest revision
    Signed-off-by: Robin Walter <hello@robinwalter.me>

--- nix/dev/flake.nix
+++ nix/dev/flake.nix
@@ -9,23 +9,23 @@
     # As a consequence, this flake only provides dependencies, and
     # we can't use the `nix` CLI as expected.
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     devenv = {
-      url = "github:cachix/devenv";
       inputs = {
         git-hooks.follows = "git-hooks";
         nixpkgs.follows = "nixpkgs";
       };
+      url = "github:cachix/devenv";
     };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
+    git-hooks = {
       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:cachix/git-hooks.nix";
     };
     nix-modules.url = "github:robinwalterfit/nix-modules";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
   };
 
   outputs = _: {
